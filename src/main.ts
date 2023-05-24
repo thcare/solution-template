2257896911f9548d6b2e240dd2f38131542042a9
Will Fowler <will.fowler@thrive.health>
2023-05-23 20:38:31 -0600

    feat: add simple solution
    this is stubbing in a solution with 0 functionality
    
    TC-17

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-export default {};
+import { Solution } from "@thcare/thcarefx";
+import patient from "./apps/patient.js";
+
+const solution: Solution = {
+  title: "Solution Template",
+  apps: {
+    patient,
+  },
+};
+
+export default solution;
