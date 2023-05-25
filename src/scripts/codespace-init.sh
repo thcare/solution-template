dddb57cf0acec75ac934be0ab5a71535b339c60e
Will Fowler <will.fowler@thrive.health>
2023-05-25 10:10:24 -0600

    feat: added codespace command for test
    this allows a solution dev to test their solution
    locally
    
    TC-17

--- src/scripts/codespace-init.sh
+++ src/scripts/codespace-init.sh
@@ -2,14 +2,14 @@
 
 npm install
 
-cd /workspaces
+cd ..
 sudo -E git clone https://github.com/thcare/web.th.care.git
 sudo -E git clone https://github.com/thcare/api.th.care.git
 sudo chown -R node:node web.th.care api.th.care
 
-cd /workspaces/web.th.care
+cd web.th.care
 sudo -E npm install
 
-cd /workspaces/api.th.care
+cd api.th.care
 sudo -E npm install
 
