3d305df209e61d80ec6c9bbb0ca5294f2deee81e
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2023-05-27 01:43:35 +0000

    fix: installation with wrong credentials
    TC-17

--- src/scripts/codespace-init.sh
+++ src/scripts/codespace-init.sh
@@ -3,13 +3,12 @@
 npm install
 
 cd ..
-sudo -E git clone https://github.com/thcare/web.th.care.git
-sudo -E git clone https://github.com/thcare/api.th.care.git
-sudo chown -R node:node web.th.care api.th.care
+git clone https://github.com/thcare/web.th.care.git
+git clone https://github.com/thcare/api.th.care.git
 
 cd web.th.care
-sudo -E npm install
+npm install
 
 cd api.th.care
-sudo -E npm install
+npm install
 
